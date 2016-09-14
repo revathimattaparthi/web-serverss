a8a84ac837b54b2977f6bb9df85019a0e5c17495
Revathii <srevathi036@gmail.com>
2016-09-14 17:51:01 +0530

    second initialization

---
+++ server2.js
@@ -0,0 +1,35 @@
+var express = require('express');
+
+var app = express();
+
+var PORT = process.env.PORT || 3000;
+
+
+var middleware = require('./middleware.js');
+
+app.use(middleware.logger);
+
+//about
+app.get('/about', middleware.requireAuthentication, function(req,res){
+
+	res.send('It is Aboutttttt pageeeeeeeeee !!!!!');
+});
+
+
+app.get('/aboutus', middleware.requireAuthentication, function(req, res){
+	res.send('It is Aboutttttttt us pageeeeeeeeee !!!!!');
+
+});
+
+app.get('/contactus', middleware.requireAuthentication, function(req, res){
+	res.send('It is Contact us page !!!!!!');
+
+});
+
+app.use(express.static(__dirname + '/html'));
+
+app.listen(PORT, function(){
+
+	console.log(' server started ' + PORT + '!');
+});
+
